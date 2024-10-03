232e57d96424f5d72304f64e8bebf6f09e9f75d5
Iagossant <iagosantanagrau@gmail.com>
2024-10-02 22:06:14 -0300

    Update Fruit Clicker.cs

--- Fruit Clicker/Fruit Clicker.cs
+++ Fruit Clicker/Fruit Clicker.cs
@@ -14,87 +14,50 @@ namespace Fruit_Clicker
     {
         public int ponto = 0, cliqueUp = 10000, cliqueSkin, segundoUp, segundoSkin, clickLvl, segundoLvl;
         Loja lj;
+        Codigo codigo;
+
         public Index()
         {
             InitializeComponent();
             lj = new Loja(this);
+            codigo = new Codigo();
             timer.Start();
         }
+
         private void pbFruit_Click(object sender, EventArgs e)
         {
             ponto += cliqueUp + cliqueSkin;
             lblPonto.Text = ponto.ToString();
         }
+
         private void timer_Tick(object sender, EventArgs e)
         {
             ponto += segundoUp + segundoSkin;
             lblPonto.Text = ponto.ToString();
         }
-        private void Interface_Click()
-    {
-        Button btn = (Button)sender;
-        string Painel = "pnl" + btn.Name.Substring(3);
-        Panel pnl = this.Controls.Find(Painel, true).FirstOrDefault() as Panel;
-        Abrir_Fechar(pnl, btn, btn.Text);
-    }
 
-    private void Abrir_Fechar(Panel pnl, Button btn, string btnName)
-    {
-        if (btn.Text == "Fechar")
-        {
-            btn.Text = btnName;
-            pnl.Enabled = false;
-            pnl.Visible = false;
-            pnl.Location = new Point(0, 0);
-        }
-        else
-        {
-            btn.Text = "Fechar";
-            pnl.Location = new Point(255, 133);
-            pnl.Enabled = true;
-            pnl.Visible = true;
-        }
-    }
-        private void btnConquista_Click(object sender, EventArgs e)
+        private void Interface_Click()
         {
             Button btn = (Button)sender;
-            Conquista cqt = new Conquista(this);
-            cqt.ShowDialog();
+            string Painel = "pnl" + btn.Name.Substring(3);
+            Panel pnl = this.Controls.Find(Painel, true).FirstOrDefault() as Panel;
+            codigo.Abrir_Fechar(pnl, btn, btn.Text);
         }
-        private void btnSkin_Click(object sender, EventArgs e)
-        {
-            lj.pnlSkin.Visible = true;
-            lj.pnlSkin.Enabled = true;
-            lj.ShowDialog();
-        }
-        private void btnBackground_Click(object sender, EventArgs e)
-        {
-            lj.ShowDialog();
-        }
+
         private void btnUpgradeClick_Click(object sender, EventArgs e)
         {
-            Upgrade(sender, ref ponto, ref clickLvl, ref cliqueUp);
+            Button btnUp = (Button)sender;
+            codigo.Upgrade(ref ponto, ref clickLvl, ref cliqueUp, btnUp);
+            lblPonto.Text = ponto.ToString();
         }
+
         private void btnUpgradeTimer_Click(object sender, EventArgs e)
         {
-            Upgrade(sender, ref ponto, ref segundoLvl, ref segundoUp);
-        }
-        private void Upgrade(object sender, ref int ponto, ref int lvl, ref int Up)
-        {
             Button btnUp = (Button)sender;
-            int preco = int.Parse(btnUp.Text);
-            if (ponto >= preco)
-            {
-                ponto -= preco;
-                preco *= 2;
-                Up++;
-                lvl++;
-                lblPonto.Text = ponto.ToString();
-                btnUp.Text = preco.ToString();
-                //inc.lblUp.Text = lvl.ToString();
-            }
-            else { new Aviso().ShowDialog(); }
+            codigo.Upgrade(ref ponto, ref segundoLvl, ref segundoUp, btnUp);
+            lblPonto.Text = ponto.ToString();
         }
+
         private void Index_FormClosed(object sender, FormClosedEventArgs e)
         {
             Properties.Settings.Default.Conquistas = null;
